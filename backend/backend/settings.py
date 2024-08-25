dd007abde7e7fa4742f13207d2aeeb7482433ff1
reanimatorum <93041144+reanimatorum@users.noreply.github.com>
2024-08-26 02:56:27 +0700

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['51.250.30.138', '127.0.0.1', 'localhost', 'thedrugojkira.zapto.org']
+ALLOWED_HOSTS = ['51.250.102.41', '127.0.0.1', '0.0.0.0', 'localhost', 'gulnara.freedynamicdns.org']
 
 
 # Application definition
